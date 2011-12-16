098f6cbaa23e11b0267f49f576a5eec23f45db6e
Joshua Priddle <jpriddle@nevercraft.net>
2011-12-16 14:58:56 -0500

    stashing my try

--- lib/rainman/driver.rb
+++ lib/rainman/driver.rb
@@ -1,107 +1,62 @@
-require 'forwardable'
-
 module Rainman
   module Driver
-    # Array of known handlers
-    def handlers
-      @handlers ||= []
-    end
 
-    # Action options
-    def options
-      @options ||= {:global => Option.new(:global)}
-    end
+    module DSL
+      def self.extended(base)
+        class << base
+          attr_accessor :actions, :handlers, :default_handler, :current_handler
+        end
 
-    # Driver actions
-    def actions
-      @actions ||= []
-    end
+        unless base.instance_variable_defined?(:@actions)
+          base.instance_variable_set(:@actions,  [])
+        end
 
-    def default_handler(value = :none)
-      if value == :none
-        @default_handler
-      else
-        @default_handler = value
+        unless base.instance_variable_defined?(:@handlers)
+          base.instance_variable_set(:@handlers, {})
+        end
       end
-    end
 
-    def current_handler
-      @current_handler ||= default_handler
-    end
-
-    # Registers a handler with the driver
-    def register_handler(name, &block)
-      add_handler(name)
-      name  = name.to_s
-      klass = "#{self.name}::#{name.to_s.camelize}"
-      if self::const_defined?(name.camelize)
-        k = klass.constantize.extend(Rainman::Handler)
-        yield k.config if block_given?
-      else
-        raise "Unknown handler '#{self}::#{name.camelize}'"
+      def included(base)
+        base.instance_variable_set(:@actions,  actions)
+        base.instance_variable_set(:@handlers, handlers)
+        base.extend(DSL)
       end
-    end
 
-    # Adds a driver method
-    def define_action(name, &block)
-      options[name] ||= Option.new(name)
-      actions << name unless actions.include?(name)
+      def register_handler(name, &block)
+        klass = "#{self.name}::#{name.to_s.camelize}".constantize
+        yield klass.config if block_given?
+        handlers[name] = klass
+      end
 
-      yield options[name] if block_given?
+      def handler_exists?(name)
+        !! handlers[name]
+      end
 
-      (class << self; self; end).class_eval do
-        define_method(name) do |*args|
-          options[:global].validate!(*args)
-          options[name].validate!(*args)
+      def set_default_handler(name)
+        @default_handler = handlers[name]
+      end
 
-          if current_handler
-            with_handler(current_handler).send(name, *args)
-          else
-            raise "no handler specified"
+      def set_current_handler(name)
+        @current_handler = handlers[name].new
+      end
+
+      def define_action(name, &block)
+        actions << name
+
+        class_eval do
+          define_method(name) do |*args|
+            if self.class.current_handler
+              self.class.current_handler.send(name, *args)
+            else
+              raise "no handler silly"
+            end
           end
         end
       end
     end
 
-    def add_option_all(opts = {})
-      options[:global].add_option opts
+    def self.extended(base)
+      base.extend(DSL)
     end
-
-    def with_handler(name, &block)
-      raise ":#{name} is not a valid handler" unless handlers.include?(name)
-      klass = "#{self.name}::#{name.to_s.camelize}".constantize.new
-      yield klass if block_given?
-      klass
-    end
-
-    def with_options(opts = {})
-      self.default_handler(opts[:default_handler]) if opts[:default_handler]
-      @prefix = opts[:prefix]
-      self
-    end
-
-    def included(base)
-      base.extend(Forwardable)
-      if @prefix
-        method_name = @prefix
-        target      = self
-        base.class_eval do
-          define_method(method_name) { target }
-        end
-      else
-        base.def_delegators self, *actions
-      end
-    end
-
-  private
-    # Records the driver handler in the @handlers array
-    def add_handler(name)
-      if handlers.include?(name)
-        raise "Handler already registered '#{self}::#{name.to_s.camelize}'"
-      else
-        handlers << name
-      end
-    end
-
   end
 end
