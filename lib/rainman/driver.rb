487baf18454bbe4c32063ba4af0dc6005484ea53
Joshua Priddle <jpriddle@nevercraft.net>
2011-12-21 12:22:09 -0500

    more docs

--- lib/rainman/driver.rb
+++ lib/rainman/driver.rb
@@ -73,15 +73,23 @@ module Rainman
       end
     end
 
-    # Return or yield an instance of the given handler.
+    # Public: Temporarily change a driver's current handler. If a block is
+    # supplied, it will be evaluated. This is useful to perform actions using
+    # multiple handlers without changing defaults.
     #
-    # Examples:
+    # name - The Symbol name of the handler to use.
     #
-    #     with_handler(:enom).transfer
+    # Examples
     #
-    #     with_handler(:enom) do |handler|
-    #       handler.transfer
-    #     end
+    #   with_handler(:enom).transfer
+    #
+    #   with_handler(:enom) do |handler|
+    #     handler.transfer
+    #   end
+    #
+    # Yields a Runner instance if a block is given.
+    #
+    # Returns a Runner instance.
     def with_handler(name)
       old_handler = current_handler
 
@@ -95,82 +103,130 @@ module Rainman
       end
     end
 
+    # DSL methods are made available as class methods in Driver modules
     module DSL
       # These methods are available in handler modules as class methods
       module PublicMethods
-        # Returns a singleton Config object
+        # Public: Alias for the Config hash
+        #
+        # Returns the Rainman::Driver::Config Hash singleton.
         def config
           Config
         end
 
-        # Returns a singleton Validations object
+        # Public: Alias for the Validations hash
+        #
+        # Returns the Rainman::Driver::Validations Hash singleton.
         def validations
           Validations
         end
       end
 
+      # Add PublicMethods to the DSL so it's automatically available to
+      # anything that is extended with Rainman::Driver
       include PublicMethods
 
-      # A hash that stores handlers
+      # Public: Registered handlers
       #
-      # Keys are the handler name (eg: :my_handler); values are the handler class
-      # (eg: MyHandler)
+      # Keys are the handler name (eg: :my_handler); values are the handler
+      # class (eg: MyHandler)
+      #
+      # Raises RuntimeError if a lookup fails.
+      #
+      # Returns a Hash.
       def handlers
         @handlers ||= Hash.new { |hash, key| raise "Invalid handler, '#{key}'" }
       end
 
       private
 
-      # Included hook; this is invoked when a Driver module is included in
-      # another class, eg:
+      # Private: Included hook; this is invoked when a Driver module is
+      # included in another class. It sets up delegation so that the including
+      # class can access a Driver's singleton methods as instance methods.
       #
-      #     class Service
-      #       include Domain
-      #     end
+      # base - The Module/Class that included this module
+      #
+      # Example
+      #
+      #   class Service
+      #     include Domain
+      #   end
+      #
+      #   s = Service.new
+      #   s.transfer #=> calls Domain.transfer
+      #
+      # Returns nothing.
       def included(base)
         base.extend(Forwardable)
         base.def_delegators self, *(instance_methods + [:with_handler])
       end
 
-      # Sets the default handler used for this Driver
+      # Private: Sets the default handler used for this Driver
+      #
+      # name - The Symbol name to set as the default handler. Should be a key
+      #        from handlers.
+      #
+      # Raises RuntimeError if a handler cannot be found.
+      #
+      # Returns the Symbol name
       def set_default_handler(name)
         @default_handler_class = handlers[name]
         @default_handler       = name
       end
 
-      # Returns this Driver's default handler
+      # Private: Get the default handler used for this Driver
+      #
+      # Returns the Symbol name of this Driver's default handler
       def default_handler
         @default_handler
       end
 
-      # Returns the name of the current handler, as an underscored symbol
+      # Private: Get the current handler in use by this Driver.
       #
-      # Example:
-      #     current_handler #=> :my_handler
+      # Returns the Symbol name of the current handler, or default handler if
+      # a current handler is not set.
       def current_handler
         @current_handler || @default_handler
       end
 
-      # Returns the current handler class (as a constant)
+      # Private: Get the current handler class.
+      #
+      # Returns the Class constant of the current handler, or the default
+      # handler if a current handler is not set.
       def current_handler_class
         @current_handler_class || @default_handler_class
       end
 
-      # A hash store containing instances of any handlers invoked
+      # Private: A hash containing handler instances. This prevents handlers
+      # from being initialized multiple times in a single session.
+      #
+      # Returns a Hash containing instances of handlers that have been
+      # initialized.
       def handler_instances
         @handler_instances ||= {}
       end
 
-      # Returns an instance of the current handler class
+      # Private: Get or set an instance of the current handler class. This
+      # method stores the instance in handler_instances, and should be used
+      # instead of manually initializing handlers.
+      #
+      # Returns an instance of the current handler class.
       def current_handler_instance
         handler_instances[current_handler] ||= current_handler_class.new
       end
 
-      # Sets the current handler. Name should be an underscored symbol
+      # Private: Sets the current handler. Name should be an underscored symbol
       # representing a class name in the current context
       #
-      # Example:
-      #     set_current_handler :my_handler
+      # name - The Symbol name of the handler to use. Can be set to nil to
+      #        clear the current handler.
+      #
+      # Example
+      #
+      #   set_current_handler :my_handler #=> sets handler to MyHandler
+      #   set_current_handler nil         #=> clears handler
+      #
+      # Returns the Symbol name of the current handler or nothing.
       def set_current_handler(name)
         if name.nil?
           @current_handler_class = @current_handler = nil
@@ -180,11 +236,19 @@ module Rainman
         end
       end
 
-      # Register a handler for use with the current driver
+      # Private: Register a handler for use with the current driver
       #
-      # Example:
+      # name - The Symbol handler name
+      # args - Arguments (unused currently)
+      #
+      # Example
+      #
       #     register_handler :bob
-      def register_handler(name, *args, &block)
+      #
+      # Yields the handler class config if a block is given.
+      #
+      # Returns the handler Class.
+      def register_handler(name, *args)
         klass = "#{self.name}::#{name.to_s.camelize}".constantize
         klass.extend(DSL::PublicMethods)
 
@@ -193,7 +257,14 @@ module Rainman
         handlers[name] = klass
       end
 
-      # Create a new namespace
+      # Private: Create a new namespace
+      #
+      # name - The Symbol handler name
+      # args - Arguments (unused currently)
+      #
+      # Yields the handler class config if a block is given.
+      #
+      # Returns a Proc.
       def namespace(name, *args, &block)
         config[name] = args
 
@@ -214,9 +285,16 @@ module Rainman
         end
       end
 
-      # Define a new action
-      def define_action(name, *args, &block)
-        define_method(name) do |*args|
+      # Private: Define a new action
+      #
+      # name - The Symbol handler name
+      # opts - Options (unused currently)
+      #
+      # Returns a Proc.
+      def define_action(name, *opts)
+        yield config[name] if block_given?
+
+        define_method(name) do |*args, &block|
           puts "Config: #{current_handler_instance.class.config}"
           runner = Runner.new(current_handler, current_handler_instance)
           runner.send(name, *args, &block)
