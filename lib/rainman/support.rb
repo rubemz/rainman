4f794da16b4d965a85a69ab4be9f78ea4a93076c
Joshua Priddle <jpriddle@me.com>
2012-01-12 11:47:42 -0500

    docs

--- lib/rainman/support.rb
+++ lib/rainman/support.rb
@@ -1,45 +1,75 @@
+# This file contains a few methods from ActiveSupport that are used by
+# Rainman. If ActiveSupport has been loaded, those methods will be used rather
+# than those in this file.
+
 # From activesupport/lib/active_support/inflector/methods.rb
 class String
-  unless respond_to?(:constantize)
-    def constantize
-      names = split('::')
-      names.shift if names.empty? || names.first.empty?
+  # Public: Convert a string into a constant. The constant must exist in
+  # ObjectSpace.
+  #
+  # Raises NameError if the constant does not exist.
+  #
+  # Returns a constant.
+  def constantize
+    names = split('::')
+    names.shift if names.empty? || names.first.empty?
 
-      constant = Object
-      names.each do |name|
-        constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
-      end
-      constant
+    constant = Object
+    names.each do |name|
+      constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
     end
-  end
+    constant
+  end unless respond_to?(:constantize)
 
-  unless respond_to?(:camelize)
-    def camelize(first_letter_in_uppercase = true)
-      if first_letter_in_uppercase
-        gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
-      else
-        self[0].chr.downcase + camelize(self)[1..-1]
-      end
+  # Public: Camel-case a string.
+  #
+  # Examples
+  #
+  #   "foo_bar"     #=> "FooBar"
+  #   "foo_bar/baz" #=> "FooBar::Baz"
+  #
+  # Returns a String.
+  def camelize(first_letter_in_uppercase = true)
+    if first_letter_in_uppercase
+      gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    else
+      self[0].chr.downcase + camelize(self)[1..-1]
     end
-  end
+  end unless respond_to?(:camelize)
 end
 
 # From lib/active_support/core_ext/hash/reverse_merge.rb
 class Hash
-  unless respond_to?(:reverse_merge)
-    def reverse_merge(other_hash)
-      other_hash.merge(self)
-    end
-  end
+  # Public: Reverse merge a hash.
+  #
+  # Example
+  #
+  #   a = { :one => :A }
+  #   b = a.reverse_merge(:one => :B, :two => :two)
+  #   a #=> { :one => :A }
+  #   b #=> { :one => :A, :two => :two }
+  #
+  # Returns a new Hash.
+  def reverse_merge(other_hash)
+    other_hash.merge(self)
+  end unless respond_to?(:reverse_merge)
 
-  unless respond_to?(:reverse_merge!)
-    def reverse_merge!(other_hash)
-      # right wins if there is no left
-      merge!( other_hash ){|key,left,right| left }
-    end
-  end
+  # Public: Reverse merge a hash in-place.
+  #
+  # Example
+  #
+  #   a = { :one => :A }
+  #   a.reverse_merge!(:one => :B, :two => :two)
+  #   a #=> { :one => :A, :two => :two }
+  #
+  # Returns a Hash.
+  def reverse_merge!(other_hash)
+    # right wins if there is no left
+    merge!( other_hash ){|key,left,right| left }
+  end unless respond_to?(:reverse_merge!)
 
   if respond_to?(:reverse_merge!) && ! respond_to?(:reverse_update)
+    # Alias Hash#reverse_update
     alias_method :reverse_update, :reverse_merge!
   end
 end
